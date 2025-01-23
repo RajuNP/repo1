a3448c4f661d4e737830fc213cae2857a9c22bf4
RajuNP <rajuraj72866@gmail.com>
2025-01-23 19:03:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef, useState } from 'react';
+import "./App.css"
+const App = () => {
+  
+  
+  return (
+    <>
+      <div className='outer-1'>
+        <div className='dropdown'>
+          <div className='dropdown-head'>
+            <img src="img."className='image'/>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
